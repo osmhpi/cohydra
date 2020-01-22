81bea3253bb4828985b891e8767af463ab73abd5
Robert Schmid <r.schmid@outlook.com>
2020-01-22 14:34:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# NS3DockerEmulator
+Network Emulator based on NS3 and Docker, with the help of NS3 docs, Docker docs and some ideas from some research papers.
+
+![NS3 Docker Emulator Schema](http://d2r9k1wfjzxupg.cloudfront.net/NS3DockerEmulatorSchema-min.png)
+
+Read the docs: https://chepeftw.github.io/NS3DockerEmulator/
+
+# Installation
+
+*Caution:* This project fiddles with your network interfaces. Please consider to use it in a virtual machine for testing purposes.
+
+There is an installation script provided, that will create a folder called `workspace` in the current working directory.
+**There is no need to clone the repo before installing.**
+The script will ask you for your password because it needs sudo permissions.
+To install all the dependencies (tested on Debian Buster), run:
+
+```
+./install.sh
+```
+
+# Examples Of Usage
+
+tbd
---
+++ pingsim.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+COUNTER=0
+MAIN_SCRIPT="main.new.py"
+
+NODES=20 # Number of nodes
+TIMEEMU=10 # Time of the emulation in seconds
+SIZE=300 # Size of the network, 300m x 300m
+SPEED=5 # Speed in m/s
+PAUSE=0 # Pause time of the nodes in seconds
+
+export NS3_HOME=$BAKE_HOME/../source/ns-3.29
+
+# We create everything
+python3 ${MAIN_SCRIPT} -n ${NODES} -t ${TIMEEMU} -s ${SIZE} -ns ${SPEED} -np ${PAUSE} create
+# We run the NS3 simulation
+python3 ${MAIN_SCRIPT} -n ${NODES} -t ${TIMEEMU} -s ${SIZE} -ns ${SPEED} -np ${PAUSE} ns3
+
+while [  $COUNTER -lt 2 ]; do
+
+    DATENOW=$(date +"%y_%m_%d_%H_%M")
+
+    # Making a backup from the last iteration's logs
+    echo "---------------------------"
+    echo ${DATENOW}
+    echo "---------------------------"
+    mkdir -p var/archive/${DATENOW}
+    mv var/log/* var/archive/${DATENOW}/
+
+    # Run the emulation
+    python3 ${MAIN_SCRIPT} -n ${NODES} -t ${TIMEEMU} -s ${SIZE} -ns ${SPEED} -np ${PAUSE} emulation
+
+    ####################################################################
+    # Run a custom script to gather data from the logs for further analysis
+    # python3 statscollector2.py bla bla bla
+    ####################################################################
+
+	let COUNTER=COUNTER+1
+done
+
+# We destroy everything cause we don't need it anymore
+python3 ${MAIN_SCRIPT} -n ${NODES} -t ${TIMEEMU} -s ${SIZE} -ns ${SPEED} -np ${PAUSE} destroy
