7b7bc607f2515e67cf40ec5632caf909bf6a6504
Martin Michaelis <code@mgjm.de>
2020-01-28 16:29:12 +0000

    Adds automatic re-execution with sudo

--- testbed/argparse.py
+++ testbed/argparse.py
@@ -2,6 +2,8 @@
 
 import argparse
 import logging
+import os
+import sys
 
 class ArgumentParser(argparse.ArgumentParser):
     """A simple-to-use argument parser for log levels.
@@ -17,7 +19,7 @@ class ArgumentParser(argparse.ArgumentParser):
         The name of the logger argument to pass to the main function.
     """
 
-    def run(self, main, setup_logging=True, logger_arg=None):
+    def run(self, main, setup_logging=True, logger_arg=None, needs_root=True):
         """Parse the arguments and pass them to a function to be called afterwards
 
         Parameters
@@ -28,6 +30,9 @@ class ArgumentParser(argparse.ArgumentParser):
             Whether to set up logging with the default python logger and parse log levels.
         logger_arg : string
             The name of the argument to pass the logger to the :code:`main` function.
+        needs_root : bool
+            Whether the program needs root privileges. The argument parser will re-execute the program with sudo
+            if code:`needs_root` is set and the current user is not root.
         """
         testbed_name = __name__.rsplit('.', 1)[0]
 
@@ -54,4 +59,58 @@ class ArgumentParser(argparse.ArgumentParser):
                 args[logger_arg] = logging.getLogger(self.prog)
                 args[logger_arg].setLevel(log_level)
 
+        logging.getLogger(self.prog).info('uid=%s', os.getuid())
+
+        if needs_root and os.getuid() != 0:
+            logging.getLogger(self.prog).info('Reexecute with sudo')
+            argv = ['sudo', '-E', 'env']
+            for k, v in os.environ.items():
+                if k.startswith('LD_') or k.endswith('_RLD') or k in self.__env_keep_manual:
+                    argv.append(f'{k}={v}')
+            argv.extend(sys.argv)
+            os.execvp(argv[0], argv)
+            # exec should never return
+            exit(1)
+
         main(**args)
+
+    # All 'Environment variables to remove' from `sudo sudo -V`.
+    # All prefix or suffix matches are removed and hardcoded.
+    # And `PATH` is missing in the list from `sudo sudo -V`.
+    __env_keep_manual = [
+        'BASH_ENV',
+        'BASHOPTS',
+        'CDPATH',
+        'ENV',
+        'FPATH',
+        'GLOBIGNORE',
+        'HOSTALIASES',
+        'IFS',
+        'JAVA_TOOL_OPTIONS',
+        'LOCALDOMAIN',
+        'NLSPATH',
+        'NULLCMD',
+        'PATH_LOCALE',
+        'PATH',
+        'PERL5DB',
+        'PERL5LIB',
+        'PERL5OPT',
+        'PERLIO_DEBUG',
+        'PERLLIB',
+        'PS4',
+        'PYTHONHOME',
+        'PYTHONINSPECT',
+        'PYTHONPATH',
+        'PYTHONUSERBASE',
+        'READNULLCMD',
+        'RES_OPTIONS',
+        'RUBYLIB',
+        'RUBYOPT',
+        'SHELLOPTS',
+        'TERMCAP',
+        'TERMINFO_DIRS',
+        'TERMINFO',
+        'TERMPATH',
+        'TMPPREFIX',
+        'ZDOTDIR',
+    ]
